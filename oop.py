adfe00cf004b13b5284455c8612f74d18c411f8c
Musa Kavuş <musakavus@gmail.com>
2022-08-29 16:37:49 +0300

    message

--- oop.py
+++ oop.py
@@ -1,2 +1,12 @@
-# NYP TEMEL PRENSİPLERİ VE İLİŞKİLER
-print('hi')
+# OOP TEMEL PRENSİPLERİ VE İLİŞKİLER
+# KALITIM
+# ÇOK BİÇİMLİLİK
+# KAPSÜLLEME
+# SOYUTLAMA
+
+"""OOP Prensiplerinde amaç
+-> Bir kodun yeniden kullanılabilirliğini sağlama
+-> Kodun  tekrar etmesini önleme
+-> Sınıflar birbiri ile ilişki kurarken olabilidğince esnek olmalı
+-> Sınıflar birbirlerine bağımlı olmamalıdır
+"""
