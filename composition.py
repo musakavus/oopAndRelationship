a50bac5b34e6454f9056ed943d7f74f48eff6dc5
Musa Kavuş <musakavus@gmail.com>
2022-08-30 19:17:04 +0300

    send

--- composition.py
+++ composition.py
@@ -11,16 +11,18 @@ class ID:
     #     return str(self.id_no)
 
 
+# Bahsettiğim üzere composition miras almak yerine bir classın elamanlarına nesne yoluyla ulaşmamıza sağlar.
+# Bu ilişkinin kurulabilmesi için
 class Person:
     def __init__(self, name, surname):  # Composition kısmı için en ideal yer constructor bölümüdür
         self.name = name
         self.surname = surname
         self.this_id = ID()  # Class içerisinde self.tihs_id isminde ID() classından referans oluşturuk.
 
-    def __str__(self):  # Bir classın standart str çıktısıdır.
+    def __str__(self):  # Bir classın standart str çıktısıdır. !!! AYNI ZAMANDA NESNEDEN STR ÇIKTISI ALMAYA YARAR !!!
         return f'{self.name} {self.surname} {str(self.this_id)}'
 
 
 p1 = Person('Mina', 'Kavuş')
 p1.this_id.create_id(184513515)
-print(p1)
+print(p1)  # ___str__ yada __repr__ metodu sayesinde classdan direk string şeklinde çıktı aldık
